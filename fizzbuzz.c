0b39e3b5cf2cca37554980b89d010b1b0537e56c
irek <irek@gabr.pl>
2023-08-09 13:20:48 +0200

    refactor

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,5 +1,6 @@
 /**
- * FizzBuzz that uses no coditions and no logical operatos.
+ * FizzBuzz without logical operations.
+ * So there are no conditions and no loops.
  * Only "write" function is used from stdlib.
  * It's so ugle, I love it <3
  *
@@ -13,20 +14,12 @@
  *	...
  */
 
-/* Hacky way to print the same instruction N times. */
-#define NTH_15(a)  a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
-#define NTH_10(a)  a;a;a;a;a;a;a;a;a;a;
-#define NTH_100(a) NTH_10(NTH_10(a))
-
-enum  str  { NUL=0,  FIZZ,   BUZZ,   FIZZBUZZ  };
-char *str[] = { "", "fizz", "buzz", "fizzbuzz" };
+#define TEN(a) a;a;a;a;a;a;a;a;a;a; /* Same instruction  10 times */
+#define HUNDRED(a) TEN(TEN(a))      /* Same instruction 100 times */
 
 /* FizzBuzz is a pattern that repeats every 15 values. */
-enum str arr[15] = {
-	NUL, NUL, FIZZ, NUL, BUZZ, FIZZ,
-	NUL, NUL, FIZZ, BUZZ, NUL, FIZZ,
-	NUL, NUL, FIZZBUZZ
-};
+char *pattern[15] = { "", "", "fizz", "", "buzz", "fizz", "",
+	"", "fizz", "buzz", "", "fizz", "", "", "fizzbuzz" };
 
 /* Return 1 if NUM is 0, otherwise return 0. */
 int
@@ -36,27 +29,26 @@ zero(char num)
 	return map[num];
 }
 
-/* Unsigned to ANSI.  Convert NUM unsigned number to number in form of
- * string in BUF buffer.  Return pointer to result string.  A lot of
- * assumptions here.  You can convert only positive number and buffer
- * has to be 8 bytes long so it fits 8-1 digit long numbers. */
+/* Convert NUM unsigned number that is 8-1 max digits long to string.
+ * Return pointer to string. */
 char *
-utoa(unsigned num, char buf[8])
+utoa(unsigned short num)
 {
+	static char buf[8] = {0};
 	int siz = 8;
 	buf[--siz] = 0;
-	NTH_15(buf[--siz] = (num % 10) + '0'; num /= 10)
-	NTH_15(siz += zero(buf[siz]-'0'))
+	TEN(buf[--siz] = (num % 10) + '0'; num /= 10)
+	TEN(siz += zero(buf[siz]-'0'))
 	return &buf[siz];
 }
 
 /* Write STR string to standard output. */
 void
-putstr(char *str)
+sput(char *str)
 {
 	int write(int, void*, int);
 	int len = 0;
-	NTH_15(len += zero(zero(str[len])))
+	TEN(len += zero(zero(str[len])))
 	write(1, str, len);
 }
 
@@ -64,11 +56,9 @@ int
 main(void)
 {
 	int i=0;
-	static char buf[8] = {0};
-
-	NTH_100(putstr(utoa(++i, buf));
-		putstr("\r");	/* Magic */
-		putstr(str[arr[(i-1)%15]]);
-		putstr("\n"))
+	HUNDRED(sput(utoa(++i));
+		sput("\r");	/* Magic */
+		sput(pattern[(i-1)%15]);
+		sput("\n"))
 	return 0;
 }
